9d8f3b91b4b6d9391e8bff0f0451797267e71365
Frederic LE BLAY <fle-blay@e3r8p1.42.fr>
2021-11-22 18:10:52 +0100

    changing ft_memcmp to work on OSX : raw not normalized result

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: fle-blay <fle-blay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 17:56:40 by fle-blay          #+#    #+#             */
-/*   Updated: 2021/11/05 11:29:18 by fle-blay         ###   ########.fr       */
+/*   Updated: 2021/11/22 18:09:40 by fle-blay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,8 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		if (((unsigned char *)s1)[i] > ((unsigned char *)s2)[i])
-			return (1);
-		if (((unsigned char *)s1)[i] < ((unsigned char *)s2)[i])
-			return (-1);
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 		i++;
 	}
 	return (0);
